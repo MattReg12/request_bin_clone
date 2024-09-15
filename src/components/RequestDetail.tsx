933fbf28630b752e63ff03c45e64471ab0b42647
Matt Register <rmregister0510@gmail.com>
2024-09-15 14:48:14 -0400

    finished markup

--- src/components/RequestDetail.tsx
+++ src/components/RequestDetail.tsx
@@ -3,7 +3,27 @@ import styles from './RequestDetail.module.css'
 
 function RequestDetail() {
   return (
-    <div className={styles.main}>
+    <div className={styles.wrapper}>
+      <p className={styles.header}>HTTP REQUEST</p>
+      <div className={styles.lineItem}>
+        <div className={styles.lineItemName}>Details</div>
+        <div className={styles.lineItemContent}>SHAZAM
+        </div>
+      </div>
+      <div className={styles.lineItem}>
+        <div className={styles.lineItemName}>Headers</div>
+        <div className={styles.lineItemContent}>
+          <span className={styles.arrow}>▶</span>
+          <a className={styles.link}>(375,000) headers</a>
+        </div>
+      </div>
+      <div className={styles.lineItemQuery}>
+        <div className={styles.lineItemName}>Query</div>
+        <div className={styles.lineItemContent}>
+          <span className={styles.arrow}>▶</span>
+          <a className={styles.link}>(3) query params</a>
+        </div>
+      </div>
 
     </div>
   )
