b64c18d062da28e8b361f91e23ef728caacccc34
Matt Register <rmregister0510@gmail.com>
2024-09-17 15:00:33 -0400

    fixed endpoint font

--- src/components/EndpointDetail.tsx
+++ src/components/EndpointDetail.tsx
@@ -7,8 +7,8 @@ interface EndpointProp {
 function EndpointDetail({ binId }: EndpointProp) {
   return (
     <div className={styles.wrapper}>
-      <p>Your endpoint is: </p>
-      <p>{`https://${binId}.x.example.com`}</p>
+      <p className={styles.p2}>Your endpoint is: </p>
+      <p className={styles.p}>{`https://${binId}.x.example.com`}</p>
     </div>
   )
 }
